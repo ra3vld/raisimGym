7268a3c41210a127755d3eede3d751a5576684a4
Dmitry Egorov <ra3vld@r3v.ru>
2019-10-23 09:54:53 -0700

    fix rsc path

--- raisim_gym/env/env/shunk_catcher/__init__.py
+++ raisim_gym/env/env/shunk_catcher/__init__.py
@@ -1,5 +1,4 @@
 import os
 from _raisim_gym import *
 
-# __SHUNK_RESOURCE_DIRECTORY__ = os.path.dirname(os.path.realpath(__file__))+'/rsc'
-__SHUNK_RESOURCE_DIRECTORY__ = "/home/ra3vld/microsoft/ws/raisimGym/raisim_gym/env/env/shunk_catcher/rsc"
+__SHUNK_RESOURCE_DIRECTORY__ = os.path.dirname(os.path.realpath(__file__))+'/rsc'
